fcc88c3d8de3168275fd8aa2cc0149d457a91742
geran1ka <roman.khoruzhy@gmail.com>
2023-09-27 07:54:24 +0300

    Cоздать класс для реализации задачи

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import {main} from './js/main';
-import {task} from './js/counter';
+import {counter} from './js/counter';
 
 
 import './index.html';
 import './scss/index.scss';
 
 main();
-console.log(task);
-console.log(task.setCounter());
-console.log(task.setName('Решено'));
+console.log(counter);
+console.log(counter.incrementCounter());
+console.log(counter.setName('Решено'));
+
+
--- src/js/counter.js
+++ src/js/counter.js
@@ -1,19 +1,27 @@
-class Task {
+class Counter {
   constructor(name, count = 0) {
     this.id = Math.floor(Math.random() * 1000);
-    this.name = name;
-    this.counter = count;
+    this._name = name;
+    this._count = count;
   }
 
-  setCounter() {
-    this.counter += 1;
+  incrementCounter() {
+    this._count += 1;
     return this;
   }
 
-  setName(newName) {
-    this.name = newName;
+  get count() {
+    return this._count;
+  }
+
+  setName(newName = '') {
+    this._name = newName;
     return this;
+  }
+
+  get name() {
+    return this._name;
   }
 }
 
-export const task = new Task ('Решить задача модуля 8 урок 3', 1);
+export const counter = new Counter ('Решить задача модуля 8 урок 3', 1);

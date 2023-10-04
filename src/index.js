58b652eb9ce2513816f63ab7ee1a3b927a81bbbd
Geran1ka <roman.khoruzhy@gmail.com>
2023-10-04 15:30:32 +0300

    Добавить функцинонал таймера

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 import {main} from './js/main';
+import {Task} from './js/counter';
+
 
 import './index.html';
 import './scss/index.scss';
+import { Timer } from './js/timer';
 
 main();
+const timer = new Timer({timerTime: 0.6, timerPause: 0.2, timerBigPause: 0.4, tasks: []});
+timer.addTask(new Task('Решить задача модуля 8 урок 3', 0));
+timer.activateTask(timer.tasks[0].id);
+
+timer.startTask(timer.tasks[0].id);
+console.log('timer: ', timer);
+// timer.startTask(timer.tasks[0].id);
+// console.log('timer: ', timer);
+// timer.startTask(timer.tasks[0].id);
+// console.log('timer: ', timer);
+
+
+
+
